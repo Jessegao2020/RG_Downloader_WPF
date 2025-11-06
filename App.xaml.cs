65dc33e40348ea57439267eb8ffad86bf9fe5534
Jesse Gao <jesse@forinx.com>
2025-11-06 09:15:14 +0800

    App (Bug Handle): Add log file when crashing

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,16 +1,43 @@
-﻿using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using RedgifsDownloader.Interfaces;
 using RedgifsDownloader.Services;
 using RedgifsDownloader.Services.DupeCleaner;
 using RedgifsDownloader.Services.Reddit;
 using RedgifsDownloader.ViewModel;
+using System.IO;
+using System.Text;
+using System.Windows;
 
 namespace RedgifsDownloader
 {
     public partial class App : Application
     {
+        private static readonly string LogDir = Path.Combine(AppContext.BaseDirectory, "logs");
+        private static readonly string LogFile = Path.Combine(LogDir, $"crash_{DateTime.Now:yyyyMMdd_HHmmss}.txt");
+
         public static IServiceProvider ServiceProvider { get; private set; }
+
+        public App()
+        {
+            this.DispatcherUnhandledException += (s, e) =>
+            {
+                LogException("DispatcherUnhandledException", e.Exception);
+                MessageBox.Show("程序崩溃，已保存日志，请查看 logs 文件夹。");
+                // 不设置 e.Handled = true，让程序自然崩溃
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                LogException("UnobservedTaskException", e.Exception);
+                throw e.Exception; // 崩溃
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                LogException("UnhandledException", e.ExceptionObject as Exception);
+                Environment.FailFast("UnhandledException", e.ExceptionObject as Exception);
+            };
+        }
 
         protected override void OnStartup(StartupEventArgs e)
         {
@@ -50,6 +77,28 @@ namespace RedgifsDownloader
             ServiceProvider = services.BuildServiceProvider();
 
             base.OnStartup(e);
+        }
+
+        private static void LogException(string type, Exception? ex)
+        {
+            try
+            {
+                Directory.CreateDirectory(LogDir);
+                string logFile = Path.Combine(LogDir, $"crash_{DateTime.Now:yyyyMMdd_HHmmss_fff}.txt"); // 加上毫秒级时间戳
+                var sb = new StringBuilder();
+                sb.AppendLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {type}");
+                if (ex != null)
+                {
+                    sb.AppendLine($"Type: {ex.GetType().FullName}");
+                    sb.AppendLine($"Message: {ex.Message}");
+                    sb.AppendLine($"StackTrace:\n{ex.StackTrace}");
+                }
+                else sb.AppendLine("Exception was null");
+
+                sb.AppendLine(new string('-', 80));
+                File.AppendAllText(logFile, sb.ToString(), Encoding.UTF8);
+            }
+            catch { }
         }
     }
 
