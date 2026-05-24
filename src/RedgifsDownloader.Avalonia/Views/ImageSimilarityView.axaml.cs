5cc6e3b4eaedb033f635933673016c242b1ff73e
Jesse Gao <jessegao2010@gmail.com>
2026-05-24 15:03:50 +0800

    imagesim

--- src/RedgifsDownloader.Avalonia/Views/ImageSimilarityView.axaml.cs
+++ src/RedgifsDownloader.Avalonia/Views/ImageSimilarityView.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using RedgifsDownloader.Avalonia.ViewModels;
+using Avalonia.Platform.Storage;
 
 namespace RedgifsDownloader.Avalonia.Views;
 
